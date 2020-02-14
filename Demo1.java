8264bfefa8466b4b86b0b0213f90e36b9f3888cb
MuKe-1024 <m15353996604@163.com>
2020-02-14 11:52:14 +0800

    Initial commit

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+package cn.tedu.Home;
+/**
+ * 2020/02/12 23:39
+ * 编写Java入门代码
+ */
+public class Demo1 {
+    public static void main(String[] args){
+        System.out.println("做一个简单的人，带一个简单的目标");
+    }
+}
